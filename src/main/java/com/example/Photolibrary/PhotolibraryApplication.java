5fa642f3155c8d89d73e3d7dd1de05a1c43d5952
Sergey Kulikov <sergokulik61@gmail.com>
2024-11-18 13:48:11 +0400

    Init project

---
+++ src/main/java/com/example/Photolibrary/PhotolibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.Photolibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotolibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotolibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Photolibrary/PhotolibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Photolibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotolibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
